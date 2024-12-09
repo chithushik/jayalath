eb481423b674ec0e9e08c3b26d6b142a25677013
chithushik <kawishnachithushi@gmail.com>
2024-12-09 23:04:33 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,11 @@
+<? php
+
+$con=new mysqli('sql205.infinityfree.com','if0_37881204','VPhTcvEaGAzdKe8','if0_37881204_addmessage');
+if(!$con){
+die(mysqli_error($con));
+
+}
+?>
+
+<!-- Read README.md to know where MySQL database password can be seen -- >
+
